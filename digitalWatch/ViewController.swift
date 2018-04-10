67f4bfc8f196a27786c512ec42eb0e365bf3750d
정순우 <soonwoo2714@gmail.com>
2018-04-10 16:10:51 +0900

    DigitalWatch_First

--- digitalWatch/ViewController.swift
+++ digitalWatch/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var timerSwitch: UISwitch!
+    
+    
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // switch off
+        timerSwitch.setOn(false, animated: true)
+        
+        //        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss a"
+        timeLabel.text = formatter.string(from: date)
+        //        timeLabel.text = String(describing: date)
     }
-
-
+    
+    @IBAction func timerStart(_ sender: Any) {
+        if timerSwitch.isOn == true {
+            //            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            
+            // closure 사용
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+                self.updateTime()
+            })
+            
+        } else {
+            myTimer.invalidate()
+            
+        }
+    }
+    
 }
 
